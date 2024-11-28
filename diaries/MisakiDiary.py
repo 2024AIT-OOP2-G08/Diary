4c8ea9ed34c1df05a5f32a163470351dbc305253
matsudakatio <77916609+matsudakatio@users.noreply.github.com>
2024-11-28 15:59:08 +0900

    MisakiDiaryゴリ押し

---
+++ diaries/MisakiDiary.py
@@ -0,0 +1,8 @@
+from  AbstractDiary import AbstractDiary
+class DiarySample(AbstractDiary):
+    def get_date(self):
+        return "2024-11-28"
+    def get_summary(self):
+        return "眠たすぎる"
+    def get_author(self):
+        return "misaki"
