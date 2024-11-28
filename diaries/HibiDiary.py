ce5f393b7abfdf7f35c1477ab3c32a2fb78a3775
ayato1027 <k22235kk@aitech.ac.jp>
2024-11-28 16:06:12 +0900

    hibi

---
+++ diaries/HibiDiary.py
@@ -0,0 +1,8 @@
+from AbstractDiary import AbstractDiary
+class HibiDiary(AbstractDiary):
+    def get_date(self):
+        return "2021-11-28"
+    def get_summary(self):
+        return "暑いようで寒い1日だった"
+    def get_author(self):
+        return "Hibi"
--- diaries/main.py
+++ diaries/main.py
@@ -1,11 +1,13 @@
 from IkedaDiary import IkedaDiary
 from MisakiDiary import MisakiDiary
 from ToriiDiary import ToriiDiary
+from HibiDiary import HibiDiary
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
     MisakiDiary(),
     IkedaDiary(),
     ToriiDiary(),
+    HibiDiary(),
     ]
 for d in diaries:
     print("---------------------------------")
