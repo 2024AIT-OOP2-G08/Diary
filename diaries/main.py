6b87f2a0975cf5f36332310a786a7835496df483
matsudakatio <77916609+matsudakatio@users.noreply.github.com>
2024-11-28 15:58:21 +0900

    Update main.py

--- diaries/main.py
+++ diaries/main.py
@@ -1,7 +1,10 @@
 from IkedaDiary import IkedaDiary
-
+from MisakiDiary import MisakiDiary
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [IkedaDiary(), ]
+diaries = [
+    MisakiDiary(),
+    IkedaDiary(),
+    ]
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
