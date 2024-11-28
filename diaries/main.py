c6f8eee25d990fe84be2b12e29a7c8aca3d3aa12
MMD-Lucky <factorytakutorii4989@gmail.com>
2024-11-28 15:50:21 +0900

    ToriiDiaryの追加及びmain.pyの修正
    日記プログラムの追加(ToriiDiary)、日記データを呼び出すメイン処理の修正(main.py)

--- diaries/main.py
+++ diaries/main.py
@@ -1,6 +1,7 @@
 from DiarySample import DiarySample
+from diaries.ToriiDiary import ToriiDiary
 # ↓のリストには、メンバーの各日記が格納されます。
-diaries = [DiarySample(), ]
+diaries = [DiarySample(), ToriiDiary(),]
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
